9190e80240c49e0a0f8395017f2df7766da82106
Morris Tai <morristai01@gmail.com>
2023-04-30 02:39:06 -0400

    chore: add example page for notion hugo testing

--- notion-hugo.config.ts
+++ notion-hugo.config.ts
@@ -3,7 +3,7 @@ import { UserConfig } from "./src/config"
 const userConfig: UserConfig = {
     mount: {
         manual: false,
-        page_url: 'https://pcloud.notion.site/Notion-DoIt-04bcc51cfe4c49938229c35e4f0a6fb6',
+        page_url: 'https://www.notion.so/morris-tai/Notion-DoIt-c6672e74cf9f405cb1090eb001ff41ec?pvs=4',
         pages: [
             // {
             //     page_id: '<page_id>',
